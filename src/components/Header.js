b31a413faa8fc215e9473bd3c94fb03c20a85df9
Harpreetpreet <preetx025@gmail.com>
2020-02-01 09:54:46 -0800

    change navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import styled from 'styled-components';
 import { Link } from 'gatsby';
 
 const Section = styled.section`
+  background: black;
+  margin-bottom: 1rem;
   padding: 1rem 1.5rem;
   font-family: ${props => props.theme.primaryFontFamily};
   .navbar {
@@ -14,12 +16,16 @@ const Section = styled.section`
     color: #b82124 !important;
   }
   .navbar-brand {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
     margin-right: 20px;
     .navbar-item img {
       max-height: 3.75rem;
     }
   }
   .navbar-menu {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
     @media screen and (max-width: 600px) {
       position: absolute;
       width: 100%;
@@ -27,6 +33,7 @@ const Section = styled.section`
     }
   }
   .navbar-item {
+    background: transparent !important;
     font-weight: 700;
     font-size: 1.2rem;
     :hover {
@@ -65,48 +72,55 @@ export default class Header extends React.Component {
 
     return (
       <Section className="section">
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-          <div className="navbar-brand">
-            <Link className="home navbar-item is-size-4 has-text-white" to="/">
-              Resurrection Remix OS
-            </Link>
-            <a
-              href="#"
-              role="button"
-              className={
-                isActive
-                  ? 'navbar-burger burger mobile is-active'
-                  : 'navbar-burger burger mobile'
-              }
-              aria-label="menu"
-              aria-expanded="false"
-              data-target="navbarBasicExample"
-              onClick={() => this.handleMobileMenu()}>
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-              <span aria-hidden="true" />
-            </a>
-          </div>
-          <div className={isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
-            <div className="navbar-end">
-              <Link to="/" className="navbar-item">
-                Screenshots
+        <div className="container">
+          <nav
+            className="navbar is-fixed-top"
+            role="navigation"
+            aria-label="main navigation">
+            <div className="navbar-brand">
+              <Link
+                className="home navbar-item is-size-4 has-text-white"
+                to="/">
+                Resurrection Remix OS
               </Link>
-              <Link to="/about" className="navbar-item">
-                Features
-              </Link>
-              <Link to="/news" className="navbar-item">
-                Downloads
-              </Link>
-              <Link to="/contact" className="navbar-item">
-                Team
-              </Link>
-              <Link to="/contact" className="navbar-item">
-                Credits
-              </Link>
+              <a
+                href="#"
+                role="button"
+                className={
+                  isActive
+                    ? 'navbar-burger burger mobile is-active'
+                    : 'navbar-burger burger mobile'
+                }
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={() => this.handleMobileMenu()}>
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+                <span aria-hidden="true" />
+              </a>
             </div>
-          </div>
-        </nav>
+            <div className={isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
+              <div className="navbar-end">
+                <Link to="/" className="navbar-item">
+                  Screenshots
+                </Link>
+                <Link to="/about" className="navbar-item">
+                  Features
+                </Link>
+                <Link to="/news" className="navbar-item">
+                  Downloads
+                </Link>
+                <Link to="/contact" className="navbar-item">
+                  Team
+                </Link>
+                <Link to="/contact" className="navbar-item">
+                  Credits
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
       </Section>
     );
   }
